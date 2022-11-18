818d9b01f47ce51362c5d47047ff906328f25577
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2022-11-18 21:15:07 +0000

    Create RestrictedYTVideosUnblocker.js

---
+++ RestrictedYTVideosUnblocker.js
@@ -0,0 +1,1 @@
+javascript:(function () {if (window.location.toString().includes('www.youtube.com/watch?v%27))%20{%20window.open(%27https://www.youtube-nocookie.com/embed/%27%20+%20window.location.toString().split(%27=%27)[1])%20}})()
