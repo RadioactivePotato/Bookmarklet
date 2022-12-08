bfc4aa3683aa20711cced74c1954f0ff7d3fb1fc
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2022-12-08 19:07:21 +0000

    Create HTMLgoggles.js

---
+++ HTMLgoggles.js
@@ -0,0 +1,1 @@
+javascript:(function () {var script=document.createElement('script');script.src='https://x-ray-goggles.mouse.org/webxray.js';script.className='webxray';script.setAttribute('data-lang','en-US');script.setAttribute('data-baseuri','https://x-ray-goggles.mouse.org');document.body.appendChild(script);}())
