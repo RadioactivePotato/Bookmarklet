95c83caf244ef8e5671b3e0940d7da30e6712ab8
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2022-12-08 18:58:23 +0000

    Create QuizletMatch0.6sec.js

---
+++ QuizletMatch0.6sec.js
@@ -0,0 +1,1 @@
+javascript:document.getElementsByClassName("UIButton UIButton--hero")[0].click(); setTimeout(function(){for(var F = setTimeout(";"), i = 0; i < F; i++) clearTimeout(i)}, 600); //Change 600
