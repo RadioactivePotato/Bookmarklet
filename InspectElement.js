b4044848a4bc46e004cc57ac274ecc156e38431b
Radioactive Potato <75719839+RadioactivePotato@users.noreply.github.com>
2022-12-12 16:19:23 +0000

    Delete InspectElement.js

--- InspectElement.js
+++
@@ -1,1 +0,0 @@
-javascript:(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
